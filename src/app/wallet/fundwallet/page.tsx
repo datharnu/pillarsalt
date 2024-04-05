f4f0bdddb23f753f9147e0aa5d12a8b8924b20d9
datharnu <olagbemisoyee@gmail.com>
2024-04-05 06:20:51 -0700

    oya noww

--- src/app/wallet/fundwallet/page.tsx
+++ src/app/wallet/fundwallet/page.tsx
@@ -13,13 +13,24 @@ import { Button } from "@/components/ui/button";
 import { X } from "lucide-react";
 import Image from "next/image";
 import bankIcon from "../../../../public/Avatar.png";
-import walletIcon from "../../../../public/Icon.png";
+import wallet from "../../../../public/Icon.png";
 import ButtonComponent from "@/components/shared/ButtonComponent";
+import React, { useState } from "react";
+
 export default function FundWallet() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const handleClose = () => {
+    setIsOpen(false);
+  };
+
   return (
     <AlertDialog>
       <AlertDialogTrigger asChild>
-        <button className="bg-[#219653] hover:bg-green-700 text-white p-1 px-2 rounded-[6px]">
+        <button
+          className="bg-[#219653] hover:bg-green-700 text-white p-1 px-2 rounded-[6px]"
+          onClick={() => setIsOpen(true)}
+        >
           Fund Wallet
         </button>
       </AlertDialogTrigger>
@@ -28,7 +39,9 @@ export default function FundWallet() {
           <AlertDialogTitle>
             <div className="flex justify-between items-center">
               <h1 className=" font-bold">Fund Wallet</h1>
-              <X />
+              <AlertDialogCancel onClick={handleClose} className="border-none">
+                <X />
+              </AlertDialogCancel>
             </div>
           </AlertDialogTitle>
           <AlertDialogDescription>
@@ -36,12 +49,12 @@ export default function FundWallet() {
               <h1 className="text-xs font-light">Wallet Balance</h1>
               <p className="text-[#056839] text-xl font-bold">₦12,750,000</p>
             </div>
-            <div className="mt-5 space-y-2">
+            <div className="mt-5 space-y-2 text-left ">
               <h1 className="">Amount</h1>
               <input type="text" className="border p-2 rounded-[8px] w-full" />
             </div>
             {/* Bank Transfer */}
-            <div className="mt-5">
+            <div className="mt-5 text-left">
               <h1>Choose Method</h1>
               <div className="flex items-center border rounded-xl my-3 px-5 gap-3 py-2">
                 <div>
@@ -58,10 +71,10 @@ export default function FundWallet() {
             </div>
 
             {/* Card Payment */}
-            <div className="mt-5">
+            <div className="mt-5 text-left">
               <div className="flex items-center border rounded-xl my-3 px-5 gap-3 py-2">
                 <div>
-                  <Image src={walletIcon} alt="bank-icon" width={90} />
+                  <Image src={wallet} alt="bank-icon" width={90} />
                 </div>
                 <div className="space-y-1">
                   <h1 className="font-bold">Card Payment</h1>
